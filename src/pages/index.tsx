a2378ff12c4e268559242208f5c7af32801ef09c
Montier Elliott <montier.billing@gmail.com>
2025-12-18 14:15:38 -0500

    lease comparison

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/settlement-demand"
+            to="/docs/lease-comparison"
           >
             Continue Reading
           </Link>
